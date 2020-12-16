8e43774bb3312a24641dec7a268b25447ecaf6ed
jayarghargh <jayarghargh@gmail.com>
2020-12-16 22:09:05 +1030

    retrieves and displays weather stats in main weather block

--- js/script.js
+++ js/script.js
@@ -1,22 +1,62 @@
 "use strict";
 // Constants
 const API_KEY = "&appid=3e6428fa21f3a15117a8b5558c08b036";
-const API_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
-const API_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const API_URL = "https://api.openweathermap.org/data/2.5/";
+const API_WEATHER_CODE = "weather?q=";
+const API_FORECAST_CODE = "forecast?q=";
+const API_UVI_CODE = "uvi?";
 const KEY_CITY = "weather_cities_searched";
-const SEARCH_LIMIT = 10;
+const SEARCH_LIMIT = 10;  // The number of searches to store in local.
+// These are required here to avoid typos when validating the values.
+const WEATHER = "weather";
+const FORECAST = "forecast";
+const UVI = "uvi";
+
 // Variables
 let cities_searched = [];
-
+let weatherStats = {temp: 0, humidity: 0, speed: 0};
 
 // Functions
-function getResponse(apiUrl) {
+function getWeatherResponse(city, expectation) {
+    // The API is hit in 3 different locations. This function handles all three.
+    let apiUrl;
+    // Adjust the url pending the required result. (Weather, forecast, or UV lookup)
+    switch (expectation) {
+        case WEATHER:
+            apiUrl = API_URL + API_WEATHER_CODE + city + API_KEY;
+            break;
+        case FORECAST:
+            apiUrl = API_URL + API_FORECAST_CODE + city + API_KEY;
+            break;
+        case UVI:
+            // UV requires lat lon in place of city. These are parsed back to this function as an array in the city parameter.
+            apiUrl = API_URL + API_UVI_CODE + "lat=" + city[0] + "&lon=" + city[1] + API_KEY;
+            break;
+    }
+
+    // The Ajax query itself.
     $.ajax({
         url: apiUrl,
         method: "GET"
     }).then(function(response) {
         // TODO Test for 404 not found.
-        displayLastSearched();
+        if (expectation === WEATHER) {
+            // Get the weather results, and store them in a global dictionary.
+            displayLastSearched();
+            weatherStats.temp = response.main.temp;
+            weatherStats.humidity = response.main.humidity;
+            weatherStats.speed = response.wind.speed;
+            // Fire off the lat lon off and get the uv index.
+            getWeatherResponse([response.coord.lat, response.coord.lon], UVI);
+        } else if (expectation === UVI) {
+            // Get the UV Index
+            weatherStats.uvindex = response.value;
+            // Compile all the stats required for the days weather, and update the page.
+            updateWeatherStats(weatherStats.temp, weatherStats.humidity, weatherStats.speed, response.value);
+        } else if (expectation === FORECAST) {
+            // Simply get the required forecast information for six days.
+            console.log(FORECAST);
+        }
     });
 }
 
@@ -61,6 +101,13 @@ function displayLastSearched() {
     });
 }
 
+function updateWeatherStats(temp, humidity, speed, uvindex) {
+    $('#deg-f').html(temp + "&deg;F");
+    $('#humidity').text(humidity + "%");
+    $('#knots').text(speed);
+    $('#uvi').text(uvindex);
+}
+
 // Statements
 // load cities
 if (retrieveCities()) {
@@ -75,14 +122,10 @@ $('#search-button').click(function (event) {
 
     // Only proceed if there's a value. Build the URL.
     if (searchCity) {
-        let immediateWeatherURL = API_WEATHER_URL + searchCity + API_KEY;
-        // let forecastWeatherURL = API_FORECAST_URL + searchCity + API_KEY;
         // Get the immediate and forecast weather reports.
-        getResponse(immediateWeatherURL);
-        // getResponse(forecastWeatherURL);
-        // Add the city to the recently searched array and store it in local.
-        // TODO Shift this away pending status of response.
+        getWeatherResponse(searchCity, WEATHER);
         storeCities(trimCityArray(cities_searched, searchCity));
+        getWeatherResponse(searchCity, FORECAST);
     }
 });
 
