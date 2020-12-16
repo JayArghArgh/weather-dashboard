d8f4f557f43a6aa9b699974fe63c0d33a536ad81
jayarghargh <jayarghargh@gmail.com>
2020-12-17 09:36:38 +1030

    implemented google places lookup

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // Constants
 const API_KEY = "&appid=3e6428fa21f3a15117a8b5558c08b036";
 const API_URL = "https://api.openweathermap.org/data/2.5/";
+const API_KEY_G_PLACES = "AIzaSyDDqTXIJDEbYWvg8V30Au2gIdtTMX5R9dI";
 const API_WEATHER_CODE = "weather?q=";
 const API_FORECAST_CODE = "forecast?q=";
 const API_UVI_CODE = "uvi?";
@@ -11,22 +12,81 @@ const SEARCH_LIMIT = 10;  // The number of searches to store in local.
 const WEATHER = "weather";
 const FORECAST = "forecast";
 const UVI = "uvi";
+const LOCATION = "location";
 
 // Variables
 let cities_searched = [];
 let weatherStats = {temp: 0, humidity: 0, speed: 0};
+let city_lookup_details;
+
+let placeSearch;
+let autocomplete;
+const componentForm = {
+    street_number: "short_name",
+    route: "long_name",
+    locality: "long_name",
+    administrative_area_level_1: "short_name",
+    country: "long_name",
+    postal_code: "short_name",
+};
 
 // Functions
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search predictions to
+    // geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("search-city"),
+        { types: ["geocode"] }
+    );
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components.
+    autocomplete.setFields(["address_component"]);
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete.addListener("place_changed", setCity);
+}
+
+function setCity() {
+    // Get the place details from the autocomplete object. and set the global variable.
+    const place = autocomplete.getPlace();
+    city_lookup_details = [
+        place.address_components[0].long_name,
+        place.address_components[1].short_name,
+        place.address_components[2].short_name
+    ]
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            const circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy,
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
+
+
 function getWeatherResponse(city, expectation) {
+    // TODO this needs to be tidied up, the city parameter is only used for UVI lat lon lookup.
     // The API is hit in 3 different locations. This function handles all three.
     let apiUrl;
+    let apiUrlExtension =  + city_lookup_details[0] + "," + city_lookup_details[1] + "," + city_lookup_details[2] + API_KEY;
     // Adjust the url pending the required result. (Weather, forecast, or UV lookup)
     switch (expectation) {
         case WEATHER:
-            apiUrl = API_URL + API_WEATHER_CODE + city + API_KEY;
+            apiUrl = API_URL + API_WEATHER_CODE + apiUrlExtension;
             break;
         case FORECAST:
-            apiUrl = API_URL + API_FORECAST_CODE + city + API_KEY;
+            apiUrl = API_URL + API_FORECAST_CODE + apiUrlExtension;
             break;
         case UVI:
             // UV requires lat lon in place of city. These are parsed back to this function as an array in the city parameter.
@@ -93,10 +153,10 @@ function displayLastSearched() {
     cities_searched.forEach(function (city, index) {
         // TODO if user clicks one of these, it should parse itself and refresh the search.
         if (index === cities_searched.length - 1) {
-            lastSearched.prepend('<a href="#!" class="collection-item blue active">' + city + '</a>')
+            lastSearched.prepend('<a href="#!" class="collection-item blue active">' + city[0] + '</a>')
         } else {
             // Everything else is standard.
-            lastSearched.prepend('<a href="#!" class="collection-item blue-text">' + city + '</a>')
+            lastSearched.prepend('<a href="#!" class="collection-item blue-text">' + city[0] + '</a>')
         }
     });
 }
@@ -117,16 +177,10 @@ if (retrieveCities()) {
 
 $('#search-button').click(function (event) {
     event.preventDefault();
-    // Trim the user input and set the variable we'll use.
-    let searchCity = $.trim($('#search-city').val());
-
-    // Only proceed if there's a value. Build the URL.
-    if (searchCity) {
-        // Get the immediate and forecast weather reports.
-        getWeatherResponse(searchCity, WEATHER);
-        storeCities(trimCityArray(cities_searched, searchCity));
-        getWeatherResponse(searchCity, FORECAST);
-    }
+    // Get the immediate and forecast weather reports.
+    getWeatherResponse("", WEATHER);
+    getWeatherResponse("", FORECAST);
+    storeCities(trimCityArray(cities_searched, city_lookup_details));
 });
 
 
