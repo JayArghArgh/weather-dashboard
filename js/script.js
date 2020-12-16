882759b5a4357cdcc76c6b4aad1019ba34fca115
jayarghargh <jayarghargh@gmail.com>
2020-12-16 20:50:43 +1030

    display last searched cities

--- js/script.js
+++ js/script.js
@@ -15,22 +15,7 @@ function getResponse(apiUrl) {
         url: apiUrl,
         method: "GET"
     }).then(function(response) {
-        $('#search-button').click(function (event) {
-            event.preventDefault();
-            // Trim the user input and set the variable we'll use.
-            let searchCity = $.trim($('#search-city').val());
-
-            // Only proceed if there's a value. Build the URL.
-            if (searchCity) {
-                let immediateWeatherURL = API_WEATHER_URL + searchCity + API_KEY;
-                let forecastWeatherURL = API_FORECAST_URL + searchCity + API_KEY;
-                // Get the immediate and forecast weather reports.
-                getResponse(immediateWeatherURL);
-                // getResponse(forecastWeatherURL);
-                // Add the city to the recently searched array and store it in local.
-                storeCities(trimCityArray(cities_searched, searchCity));
-            }
-        });
+        displayLastSearched();
     });
 }
 
@@ -58,10 +43,44 @@ function trimCityArray(cityArrayToTrim, cityToAdd) {
     return cityArrayToTrim;
 }
 
+function displayLastSearched() {
+    // Display the last searched cities for the user, in reverse order.
+    let lastSearched = $('#last-searched');
+    lastSearched.empty();
+    $('#search-city').val("");
+    // The last item should be the current search.
+    cities_searched.forEach(function (city, index) {
+        if (index === cities_searched.length - 1) {
+            lastSearched.prepend('<a href="#!" class="collection-item blue active">' + city + '</a>')
+        } else {
+            // Everything else is standard.
+            lastSearched.prepend('<a href="#!" class="collection-item blue-text">' + city + '</a>')
+        }
+    });
+}
+
 // Statements
 // load cities
 if (retrieveCities()) {
     cities_searched = retrieveCities();
+    displayLastSearched();
 }
+
+$('#search-button').click(function (event) {
+    event.preventDefault();
+    // Trim the user input and set the variable we'll use.
+    let searchCity = $.trim($('#search-city').val());
+
+    // Only proceed if there's a value. Build the URL.
+    if (searchCity) {
+        let immediateWeatherURL = API_WEATHER_URL + searchCity + API_KEY;
+        // let forecastWeatherURL = API_FORECAST_URL + searchCity + API_KEY;
+        // Get the immediate and forecast weather reports.
+        getResponse(immediateWeatherURL);
+        // getResponse(forecastWeatherURL);
+        // Add the city to the recently searched array and store it in local.
+        storeCities(trimCityArray(cities_searched, searchCity));
+    }
+});
 
 
